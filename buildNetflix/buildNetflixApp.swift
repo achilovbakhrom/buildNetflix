d0ea8357a4250321fb5a6e5df867f267a89e5c17
mailbak36@gmail.com <mailbak36@gmail.com>
2023-12-01 22:03:13 +0500

    Initial commit

---
+++ buildNetflix/buildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildNetflixApp.swift
+//  buildNetflix
+//
+//  Created by Bakhrom Achilov on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct buildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
